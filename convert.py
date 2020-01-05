eb9f394ea6cb21729dbd38011369852be10a6b67
Zachary Itskin <36995254+Zayitskin@users.noreply.github.com>
2020-01-04 20:56:33 -0600

    Added arbitrary sample id support
    TODO: Refuse sample ids that are too big

--- convert.py
+++ convert.py
@@ -6,36 +6,36 @@ from collections import defaultdict as ddict
 EMPTY_COMMAND = 0
 STRUCT = struct.Struct(">I")
 
-def parse(msg):
+def parse(msg, sample = 1):
 
     msg = msg.dict()
     if msg["type"] == "note_on":
-        return convert_note_on(msg["note"]), convert_volume(msg["velocity"])
+        return convert_note_on(msg["note"], sample), convert_volume(msg["velocity"], sample)
     elif msg["type"] == "note_off":
-        return convert_note_off(msg["note"]), convert_volume(msg["velocity"])
+        return convert_note_off(msg["note"], sample), convert_volume(msg["velocity"], sample)
     else:
         return None, None
     
-def convert_note_on(note):
+def convert_note_on(note, sample):
 
     msg = 1 << 31   # get_next_command = 1
-    msg |= 1 << 24  # sample_id = 1
+    msg |= sample << 24  # sample_id = 1
     msg |= 1 << 16  # command_id = 1 (kon)
     msg |= note     # operand = note
     return msg
 
-def convert_note_off(note):
+def convert_note_off(note, sample):
 
     msg = 1 << 31   # get_next_command = 1
-    msg |= 1 << 24  # sample_id = 1
-    msg |= 2 << 16  # command_id = 2 (kon)
+    msg |= sample << 24  # sample_id = 1
+    msg |= 2 << 16  # command_id = 2 (koff)
     msg |= note     # operand = note
     return msg
 
-def convert_volume(vol):
+def convert_volume(vol, sample):
 
     msg = 1 << 31   # get_next_command = 1
-    msg |= 1 << 24  # sample_id = 1
+    msg |= sample << 24  # sample_id = 1
     msg |= 6 << 16  # command_id = 6 (volB)
     msg |= vol      # operand = vol
     return msg
